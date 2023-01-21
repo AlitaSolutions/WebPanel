69edf64502d9b291141b00adc464c6db090df809
Koorosh <mr.oplus@yahoo.fr>
2023-01-21 22:30:26 +0330

    schema service

--- controllers/admin/schema.controller.js
+++ controllers/admin/schema.controller.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const db = require('../../db');
-const mongo = require("mongodb");
 const {body, validationResult, param} = require("express-validator");
+const SchemaService = require("../../services/schema.service");
+const ErrorHandler = require("../../errors/error.handler");
 class SchemaController{
     constructor(){
         router.get('/', this.getSchemas);
@@ -43,25 +43,21 @@ class SchemaController{
     }
      */
     async getSchemas(req, res){
-        const schemas = await db.schemas().find({}).toArray();
-        res.status(200).json(schemas);
+        res.status(200).json({data: SchemaService.getSchema() });
     }
     async createSchema(req,res){
         const errors = validationResult(req);
         if(!errors.isEmpty()){
-            return res.status(400).json({errors: errors.array()});
+           return ErrorHandler.handle(res, errors.array());
         }
-        const {name , fields} = req.body;
-        //check if schema exists
-        let schema = await db.schemas().findOne({name});
-        if(schema){
-            return res.status(409).json({message: 'Schema already exists'});
-        }else{
-            schema = await db.schemas().insertOne({name, fields});
+        try {
+            const schema = await SchemaService.createSchema(req.body);
             res.status(200).json({
                 message: 'Schema created',
-                data: await db.schemas().findOne({_id: schema.insertedId}),
+                data: schema,
             });
+        }catch (e) {
+            ErrorHandler.handle(res, e);
         }
     }
     async updateSchema(req,res){
@@ -70,21 +66,23 @@ class SchemaController{
             return res.status(400).json({errors: errors.array()});
         }
         const {id} = req.params;
-        const {name, fields} = req.body;
-        const schema = await db.schemas().findOneAndUpdate({_id: new mongo.ObjectId(id)}, {$set: {name, fields}});
-        if(schema.value){
-            res.status(200).json({message: 'Schema updated' , data : await db.schemas().findOne({_id: new mongo.ObjectId(id)})});
-        }else{
-            res.status(500).json({message: 'Unable to update schema' , data: schema.lastErrorObject});
+        try {
+            const schema = await SchemaService.updateSchema(id, req.body);
+            res.status(200).json({
+                message: 'Schema updated',
+                data: schema,
+            });
+        }catch (e) {
+            ErrorHandler.handle(res, e);
         }
     }
     async deleteSchema(req,res){
         const {id} = req.params;
-        const schema = await db.schemas().deleteOne({_id: new mongo.ObjectId(id)});
-        if(schema.deletedCount === 1){
+        try{
+            await SchemaService.deleteSchema(id);
             res.status(200).json({message: 'Schema deleted'});
-        }else{
-            res.status(404).json({message: 'Schema not found'});
+        }catch (e){
+            ErrorHandler.handle(res, e);
         }
     }
 }
---
+++ services/schema.service.js
@@ -0,0 +1,38 @@
+const db = require("../db");
+const ConflictError = require("../errors/conflict.error");
+const mongo = require("mongodb");
+const InternalError = require("../errors/internal.error");
+
+class SchemaService {
+    static async getSchema() {
+        return await db.schemas().find({}).toArray();
+    }
+    static async createSchema(body){
+        const {name , fields} = body;
+        //check if schema exists
+        let schema = await db.schemas().findOne({name});
+        if(schema) {
+            throw new ConflictError('Schema already exists');
+        }
+        schema = await db.schemas().insertOne({name, fields});
+        return schema;
+    }
+    static async updateSchema(id,body){
+        const {name, fields} = body;
+        const schema = await db.schemas().findOneAndUpdate({_id: new mongo.ObjectId(id)}, {$set: {name, fields}});
+        if(schema.value){
+            return await db.schemas().findOne({_id: new mongo.ObjectId(id)});
+        }else{
+            throw new InternalError('Unable to update schema');
+        }
+    }
+    static async deleteSchema(id){
+        const schema = await db.schemas().deleteOne({_id: new mongo.ObjectId(id)});
+        if(schema.deletedCount === 0){
+            throw new InternalError('Unable to delete schema');
+        }
+        return schema;
+    }
+}
+
+module.exports = SchemaService;
